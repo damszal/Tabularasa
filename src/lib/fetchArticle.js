dfdada5db8227f44d99a9611f642c31e582ed7d8
damian <damian.szalimow@onet.eu>
2024-11-24 13:35:40 +0100

    cms connection

--- src/lib/fetchArticle.js
+++ src/lib/fetchArticle.js
@@ -1,3 +1,19 @@
+import { createClient, OAuthStrategy } from "@wix/sdk";
+import { items } from "@wix/data";
 
+const WixClient = createClient({
+    modules: { items},
+    auth: OAuthStrategy({
+        clientId: import.meta.env.WIX_CLIENT_SECRET,
+    }), 
+})
+
+export default async function fetchArticles() {
+    let query = WixClient.items.queryDataItems({
+        dataCollectionId: "examples",
+    });
+    const articles = await query.find()
+    return articles.items
+}
   
   
